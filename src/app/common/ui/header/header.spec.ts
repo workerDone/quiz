caad50130881edf4bcda57d12264751e24e27955
Mykola Chmut <developersame@gmail.com>
2026-01-08 14:09:47 +0200

    Wip

--- src/app/common/ui/header/header.spec.ts
+++ src/app/common/ui/header/header.spec.ts
@@ -1,9 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { expect } from 'vitest';
+import { expect, vi } from 'vitest';
 import { By } from '@angular/platform-browser';
-import { provideRouter, RouterLink } from '@angular/router';
+import { provideRouter, Router } from '@angular/router';
 import { Header } from './header';
-import { Component } from '@angular/core';
+import { Component, DebugElement } from '@angular/core';
 
 @Component({
   selector: 'app-default',
@@ -15,16 +15,18 @@ export class Mock {
 describe('Header', () => {
   let component: Header;
   let fixture: ComponentFixture<Header>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Header, RouterLink],
+      imports: [Header],
       providers: [provideRouter([])],
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(Header);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     await fixture.whenStable();
   });
 
@@ -47,5 +49,20 @@ describe('Header', () => {
     const text: HTMLElement = fixture.debugElement.query(By.css('.default-text')).nativeElement;
     expect(text).toBeTruthy();
     expect(text.textContent).toContain('Just do it');
+  });
+
+  it('should have a link to root', () => {
+    const logoLink: DebugElement = fixture.debugElement.query(By.css('.logo'));
+    expect(logoLink).toBeTruthy();
+    expect(logoLink.attributes['routerLink']).toEqual("/");
+  });
+
+  it('should navigate to root on click', async () => {
+    const navigateSpy = vi.spyOn(router, 'navigateByUrl').mockResolvedValue(true);
+    const linkElement = fixture.debugElement.query(By.css('.logo'));
+    expect(linkElement).toBeTruthy();
+    linkElement.nativeElement.click();
+    fixture.detectChanges();
+    expect(navigateSpy).toHaveBeenCalledTimes(1);
   });
 });
